6ae701013b88c7927bfadb05d7552f5585d8de74
Mohammad Wasi <reallywasi@gmail.com>
2023-09-21 11:47:58 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public Book(Book other) {
+        this.title = other.title;
+        this.author = other.author;
+        this.publicationYear = other.publicationYear;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+
+    public static void main(String[] args) {
+        
+        Book book1 = new Book("Mein Kamf", "Fuhrer", 1913);
+
+        Book book2 = new Book(book1);
+        System.out.println("Details of Book 1:");
+        book1.displayDetails();
+        System.out.println("\nDetails of Book 2 (Copy of Book 1):");
+        book2.displayDetails();
+    }
+}
